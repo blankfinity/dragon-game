ab7eb030cd908d5a76acc5dc66f08b760f685999
blankfinity <35239723+blankfinity@users.noreply.github.com>
2018-01-09 18:39:15 +0500

    SettingsUpdate

--- Dragon Tails/Assets/_Project/Common/Scripts/SettingsScript.cs
+++ Dragon Tails/Assets/_Project/Common/Scripts/SettingsScript.cs
@@ -2,25 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class SettingsScript : MonoBehaviour {
 
     public float volume;
     public int GraphicsQualityIndex;
     public AudioMixer audioMixer;
-
+    public Dropdown QualitySettingDropdown;
 	// Use this for initialization
 	void Start () {
-		
-	}
+        GraphicsQualityIndex = QualitySettings.GetQualityLevel();
+        QualitySettingDropdown.value = GraphicsQualityIndex;
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
 
-    public void SetVolume()
+    public void SetVolume(float Volume)
     {
+        this.volume = Volume;
         audioMixer.SetFloat("Volume", volume);
     }
 
